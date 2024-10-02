9391781c2fba69fa705154d53596e4ed878927b4
North Harrington <339713067@GoTVDSB.ca>
2024-10-02 09:40:35 -0400

    Add project files.

---
+++ 5.3/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _5._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sky;
+            int score = 0;
+            string anwser2;
+            string anwser3;
+            string anwser4;
+
+            Console.WriteLine("Hey today you will be taking a quiz, good luck");
+            Console.WriteLine("Question 1 what colour is the sky?");
+            sky = Console.ReadLine().ToLower();
+
+            if (sky == "blue")
+            {
+
+
+                Console.WriteLine(" 1 for 1 good job");
+                score += 1;
+                
+
+            }
+            Console.WriteLine();
+            Console.WriteLine(" Your score is " + score);
+            
+         
+
+
+        }
+    }
+}
