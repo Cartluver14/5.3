e8f3c4679c2183db3beed9877e7da8ae4ef71691
North Harrington <339713067@GoTVDSB.ca>
2024-10-03 09:25:58 -0400

    done

--- 5.3/Program.cs
+++ 5.3/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace _5._3
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace _5._3
 {
     internal class Program
     {
@@ -6,7 +8,7 @@
         {
             string sky;
             int score = 0;
-            string anwser2;
+            int anwser2;
             string anwser3;
             string anwser4;
 
@@ -23,10 +25,76 @@
                 
 
             }
+            else
+            {
+                Console.WriteLine("Incorect");
+            }
             Console.WriteLine();
             Console.WriteLine(" Your score is " + score);
+
+
+            Console.WriteLine("Question 2 What year did covid 19 start?");
+
+            int.TryParse(Console.ReadLine(), out anwser2);
+
+            if (anwser2 == 2020)
+            {
+
+                Console.WriteLine("2 for 2 your doing great");
+                score += 1;
+            }
+            else
+            {
+                Console.WriteLine("Incorect");
+            }
+            Console.WriteLine();
+            Console.WriteLine(" Your score is " + score);
+
+            Console.WriteLine("question 3 what is the capital of canada?");
+
+            anwser3 = Console.ReadLine().ToLower();
+
+            if (anwser3 == "ottawa")
+            {
+
+
+                Console.WriteLine("3 for 3 your doing great");
+                score += 1;
+            }
+            else
+            {
+                Console.WriteLine("incorect");
+            }
+            Console.WriteLine();
+            Console.WriteLine(" Your score is " + score);
+
+            Console.WriteLine("question 4 what colour is the leaf on the canidain flag?         " +
+                "A. blue " +
+                "B. orange " +
+                "C. red " +
+                "D. white ");
+
+            anwser4 = Console.ReadLine().ToLower();
+
+            if (anwser4 == "red")
+            {
+
+
+                Console.WriteLine("4 for 4 your doing great");
+                score += 1;
+            }
+            else
+            {
+                Console.WriteLine("wrong!!");
+            }
+            Console.WriteLine("The quiz is done good work thanks!");
             
-         
+            Console.WriteLine(" Your final score for the quiz is " + score);
+
+
+
+
+
 
 
         }
